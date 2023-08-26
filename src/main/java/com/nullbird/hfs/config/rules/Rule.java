2e2db3dfda78071a504fa97f58485c5bf76ff6be
Pierre Grimaud <pieroxy@pieroxy.net>
2023-08-26 14:28:12 +0200

    📝 Rule has javadoc

--- src/main/java/com/nullbird/hfs/config/rules/Rule.java
+++ src/main/java/com/nullbird/hfs/config/rules/Rule.java
@@ -5,15 +5,35 @@ import com.nullbird.hfs.utils.config.RuleAction;
 import com.nullbird.hfs.utils.config.RuleMatcher;
 import com.nullbird.hfs.utils.errors.ConfigurationException;
 
+/**
+ * A rule contains a {@link #matcher} and an {@link #action}. When executed, the engine will check if the rule matches
+ * the current request being processed, by calling the matcher. If so, the action will be run. If not, the following
+ * rule will then be checked.
+ */
 public class Rule {
-  private RuleMatcher matcher;
-  private RuleAction action;
+  /**
+   * The {@link RuleMatcher} that will decide if the {@link #action} should be run.
+   */
+  protected RuleMatcher matcher;
+  /**
+   * The {@link RuleAction} that will be run if the  {@link #matcher} matches the request.
+   */
+  protected RuleAction action;
+
+  /**
+   * Initializes the {@link #matcher} and the {@link #action} configured.
+   * @param config The global configuration object
+   * @throws ConfigurationException if some parameters are missing, defined to incorrect values, incompatibles, etc.
+   */
 
   public void init(Config config) throws ConfigurationException {
     matcher.initialize(config);
     action.initialize(config);
   }
 
+  /**
+   * Closes the {@link #matcher} and the {@link #action} configured.
+   */
   public void close() {
     try {
       matcher.stop();
@@ -27,18 +47,22 @@ public class Rule {
     }
   }
 
+  /** */
   public RuleMatcher getMatcher() {
     return matcher;
   }
 
+  /** */
   public void setMatcher(RuleMatcher matcher) {
     this.matcher = matcher;
   }
 
+  /** */
   public RuleAction getAction() {
     return action;
   }
 
+  /** */
   public void setAction(RuleAction action) {
     this.action = action;
   }
