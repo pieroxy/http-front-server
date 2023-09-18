0d6f332a3c45489c3905caa09a8c15a8a55ffab2
Pierre Grimaud <pieroxy@pieroxy.net>
2023-09-18 16:04:38 +0200

    ✨ Server hot reloads certificates. This is a first version.
    We should also be able to:
    - Handle full configuration reload
    - Reload https certs without any interruption

--- src/main/java/com/nullbird/hfs/bootstrap/TomcatBuilder.java
+++ src/main/java/com/nullbird/hfs/bootstrap/TomcatBuilder.java
@@ -13,6 +13,8 @@ import org.apache.tomcat.util.net.SSLHostConfig;
 import org.apache.tomcat.util.net.SSLHostConfigCertificate;
 
 import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -59,29 +61,80 @@ public class TomcatBuilder {
 
   }
 
+  Connector sslConnector;
   void addHttpsConnector(Config config) {
     TomcatSslConfig hfsSslConf = config.getTomcatConfig().getSslConfig();
     if (hfsSslConf!=null && hfsSslConf.getPort()>0) {
-      Connector ctr = new Connector();
-      ctr.setPort(hfsSslConf.getPort());
-      ctr.setSecure(true);
-      ctr.setScheme("https");
-      ctr.setProperty("SSLEnabled", "true");
-      setCommonConnectorAttributes(ctr, config);
-
-      SSLHostConfig sslConfig = new SSLHostConfig();
-      sslConfig.setCiphers(hfsSslConf.getCiphers());
-      sslConfig.setProtocols(hfsSslConf.getProtocols());
-
-      SSLHostConfigCertificate certConfig = new SSLHostConfigCertificate(sslConfig, SSLHostConfigCertificate.Type.RSA);
-      certConfig.setCertificateFile(hfsSslConf.getCertificateFile());
-      certConfig.setCertificateKeyFile(hfsSslConf.getCertificateKeyFile());
-      certConfig.setCertificateChainFile(hfsSslConf.getCertificateChainFile());
-      sslConfig.addCertificate(certConfig);
-
-      ctr.addSslHostConfig(sslConfig);
-      tomcat.setConnector(ctr);
+      sslConnector = buildSslConnector(config);
+      tomcat.setConnector(sslConnector);
+      scheduleCertListener(tomcat, config);
     }
+  }
+
+  private Connector buildSslConnector(Config config) {
+    TomcatSslConfig hfsSslConf = config.getTomcatConfig().getSslConfig();
+    Connector ctr = new Connector();
+    ctr.setPort(hfsSslConf.getPort());
+    ctr.setSecure(true);
+    ctr.setScheme("https");
+    ctr.setProperty("SSLEnabled", "true");
+    setCommonConnectorAttributes(ctr, config);
+
+    SSLHostConfig sslConfig = new SSLHostConfig();
+    sslConfig.setCiphers(hfsSslConf.getCiphers());
+    sslConfig.setProtocols(hfsSslConf.getProtocols());
+
+    SSLHostConfigCertificate certConfig = new SSLHostConfigCertificate(sslConfig, SSLHostConfigCertificate.Type.RSA);
+    certConfig.setCertificateFile(hfsSslConf.getCertificateFile());
+    certConfig.setCertificateKeyFile(hfsSslConf.getCertificateKeyFile());
+    certConfig.setCertificateChainFile(hfsSslConf.getCertificateChainFile());
+    sslConfig.addCertificate(certConfig);
+
+    ctr.addSslHostConfig(sslConfig);
+    return ctr;
+  }
+
+  private void scheduleCertListener(Tomcat tomcat, Config config) {
+    TomcatSslConfig hfsSslConf = config.getTomcatConfig().getSslConfig();
+    Timer timer = new Timer();
+    File [] files = new File[] {
+            new File(hfsSslConf.getCertificateFile()),
+            new File(hfsSslConf.getCertificateKeyFile()),
+            new File(hfsSslConf.getCertificateChainFile())
+    };
+    long []timestamps = new long[] {
+            files[0].lastModified(),
+            files[1].lastModified(),
+            files[2].lastModified()
+    };
+    timer.scheduleAtFixedRate(new TimerTask() {
+      @Override
+      public void run() {
+        try {
+          if (files[0].lastModified() != timestamps[0] ||
+                  files[1].lastModified() != timestamps[1] ||
+                  files[2].lastModified() != timestamps[2]) {
+            LOGGER.warning("Reloading SSL certificate");
+            try {
+              Thread.sleep(1000); // Feeble attempt to leave some time to whatever process to copy all the files.
+              tomcat.getEngine().getService().removeConnector(sslConnector);
+              Connector ctr = buildSslConnector(config);
+              sslConnector.destroy();
+              tomcat.setConnector(ctr);
+              sslConnector = ctr;
+              timestamps[0] = files[0].lastModified();
+              timestamps[1] = files[1].lastModified();
+              timestamps[2] = files[2].lastModified();
+            } catch (Exception e) {
+              tomcat.setConnector(sslConnector);
+              LOGGER.log(Level.SEVERE, "Trying to apply the new certificates", e);
+            }
+          }
+        } catch (Exception e) {
+          LOGGER.log(Level.SEVERE, "Watching for new certs", e);
+        }
+      }
+    }, 5000, 5000);
   }
 
   private void addMainContext(TomcatConfig tomcatConfig) {
