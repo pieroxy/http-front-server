pieroxy/http-front-server

commit 52fb520a8f441628e190ffac29081ee7d7b92861

🐛 More than one cookie can be present under the same name

src/main/java/com/nullbird/hfs/config/rules/actions/BasicAuthenticate.java
@@ -64,7 +64,7 @@ public class BasicAuthenticate implements RuleAction {
 
   @Override
   public void run(HttpRequest request, HttpResponse response, RuleMatcher matcher) {
-    if (Objects.equals(request.getCookieValue(cookieName), COOKIE_VALUE)) return;
+    if (request.getCookieValues(cookieName).contains(COOKIE_VALUE)) return;
     try {
       Map<String, String> postData = request.decodeSimpleXWWWFormUrlEncodedPostData();
       String login = postData.get(AUTH_LOGIN);

src/main/java/com/nullbird/hfs/config/rules/matchers/ContainsCookie.java
@@ -1,10 +1,11 @@
 package com.nullbird.hfs.config.rules.matchers;
 
 import com.nullbird.hfs.config.Config;
-import com.nullbird.hfs.utils.config.RuleMatcher;
 import com.nullbird.hfs.http.HttpRequest;
+import com.nullbird.hfs.utils.config.RuleMatcher;
 import com.nullbird.hfs.utils.errors.ConfigurationException;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -31,8 +32,11 @@ public class ContainsCookie  implements RuleMatcher {
    */
   @Override
   public boolean match(HttpRequest request) {
-    String cookieValue = request.getCookieValue(this.name);
-    return cookieValue != null && pattern.matcher(cookieValue).find();
+    List<String> cookieValues = request.getCookieValues(this.name);
+    for (var cookieValue : cookieValues) {
+      if (pattern.matcher(cookieValue).find()) return true;
+    }
+    return false;
   }
 
   @Override

src/main/java/com/nullbird/hfs/http/HttpRequest.java
@@ -3,6 +3,7 @@ package com.nullbird.hfs.http;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Map;
 
 public interface HttpRequest {
@@ -10,7 +11,13 @@ public interface HttpRequest {
   String getUrl();
   String getHeader(String name);
   String getParameter(String name);
-  String getCookieValue(String name);
+
+  /**
+   *
+   * @param name
+   * @return never null
+   */
+  List<String> getCookieValues(String name);
 
   byte[] getBodyAsByteArray() throws IOException;
 

src/main/java/com/nullbird/hfs/http/ServletHttpRequest.java
@@ -1,7 +1,6 @@
 package com.nullbird.hfs.http;
 
 import com.nullbird.hfs.utils.StreamUtils;
-import com.nullbird.hfs.utils.UtilityCollectors;
 import jakarta.servlet.AsyncContext;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,6 +14,7 @@ import java.net.URLDecoder;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class ServletHttpRequest implements HttpRequest {
   private final static Logger LOGGER = Logger.getLogger(ServletHttpRequest.class.getName());
@@ -106,14 +106,14 @@ public class ServletHttpRequest implements HttpRequest {
   }
 
   @Override
-  public String getCookieValue(String name) {
+  public List<String> getCookieValues(String name) {
     if (LOGGER.isLoggable(Level.FINER)) LOGGER.finer("getCookieValue("+name+") :: not computing");
     var cookies = request.getCookies();
     if (cookies == null) return null;
-    Cookie c = Arrays.stream(cookies)
+    return Arrays.stream(cookies)
             .filter(cookie -> Objects.equals(cookie.getName(), name))
-            .collect(UtilityCollectors.getOneItemOrNull());
-    return c == null ? null : c.getValue();
+            .map(Cookie::getValue)
+            .collect(Collectors.toList());
   }
 
   @Override

src/main/java/com/nullbird/hfs/utils/UtilityCollectors.java
@@ -7,14 +7,23 @@ public class UtilityCollectors {
   // From https://stackoverflow.com/questions/22694884/filter-java-stream-to-1-and-only-1-element
   public static <T> Collector<T, ?, T> getOneItemOrNull() {
     return Collectors.collectingAndThen(
-        Collectors.toList(),
-        list -> {
-            if (list.size()==0) return null;
-          if (list.size() != 1) {
-            throw new IllegalStateException();
-          }
-          return list.get(0);
-        }
+            Collectors.toList(),
+            list -> {
+              if (list.size()==0) return null;
+              if (list.size() != 1) {
+                throw new IllegalStateException();
+              }
+              return list.get(0);
+            }
+    );
+  }
+  public static <T> Collector<T, ?, T> getFirstItemOrNull() {
+    return Collectors.collectingAndThen(
+            Collectors.toList(),
+            list -> {
+              if (list.size()==0) return null;
+              return list.get(0);
+            }
     );
   }
 }

commit 8339ed0440279fdd1cf576085e4fca106c51a89d

🐛 Regression from commit 52fb520a

src/main/java/com/nullbird/hfs/config/rules/actions/BasicAuthenticate.java
@@ -29,6 +29,7 @@ import java.util.logging.Logger;
  */
 public class BasicAuthenticate implements RuleAction {
   private static final Logger LOGGER = Logger.getLogger(BasicAuthenticate.class.getName());
+  private static final Level DEBUG_LEVEL = Level.FINE;
   public final String COOKIE_VALUE="OK";
   public final String AUTH_LOGIN=this.getClass().getName() + ".login";
   public final String AUTH_PASSWORD=this.getClass().getName() + ".password";
@@ -64,7 +65,13 @@ public class BasicAuthenticate implements RuleAction {
 
   @Override
   public void run(HttpRequest request, HttpResponse response, RuleMatcher matcher) {
-    if (request.getCookieValues(cookieName).contains(COOKIE_VALUE)) return;
+    var cookieValues = request.getCookieValues(cookieName);
+    if (cookieValues!=null && cookieValues.contains(COOKIE_VALUE)) {
+      if (LOGGER.isLoggable(DEBUG_LEVEL)) {
+        LOGGER.log(DEBUG_LEVEL, "Letting request go through - " + request.getUrl());
+      }
+      return;
+    }
     try {
       Map<String, String> postData = request.decodeSimpleXWWWFormUrlEncodedPostData();
       String login = postData.get(AUTH_LOGIN);
@@ -79,24 +86,23 @@ public class BasicAuthenticate implements RuleAction {
       }
 
       if (login != null && password != null && target != null && request.getMethod().equals("POST")) {
-        if (LOGGER.isLoggable(Level.FINE)) LOGGER.fine("Authenticating the request");
+        if (LOGGER.isLoggable(DEBUG_LEVEL)) LOGGER.log(DEBUG_LEVEL, "Authenticating the request - " + request.getUrl());
         if (Objects.equals(credentials.get(login), password)) {
-          if (LOGGER.isLoggable(Level.FINE)) LOGGER.fine("Authentication successful");
+          if (LOGGER.isLoggable(DEBUG_LEVEL)) LOGGER.log(DEBUG_LEVEL, "Authentication successful - " + request.getUrl());
           // Authenticating the user
           authenticate(response);
           response.sendRedirect(HttpServletResponse.SC_FOUND, target);
         } else {
-          if (LOGGER.isLoggable(Level.FINE)) LOGGER.fine("Authentication failed");
+          if (LOGGER.isLoggable(Level.INFO)) LOGGER.info("Authentication failed - " + request.getUrl());
           failLogin(request, target, response);
         }
       } else {
-        if (LOGGER.isLoggable(Level.FINE)) LOGGER.fine("Request needs authentication");
+        if (LOGGER.isLoggable(DEBUG_LEVEL)) LOGGER.log(DEBUG_LEVEL, "Request needs authentication - " + request.getUrl());
         showLoginPrompt(request, response);
       }
     } catch (IOException e) {
-      if (LOGGER.isLoggable(Level.FINE)) LOGGER.fine("Request failed decoding and thus needs authentication");
+      if (LOGGER.isLoggable(Level.INFO)) LOGGER.log(Level.INFO, "Request failed decoding and thus needs authentication - " + request.getUrl(), e);
       showLoginPrompt(request, response);
-
     }
   }
 