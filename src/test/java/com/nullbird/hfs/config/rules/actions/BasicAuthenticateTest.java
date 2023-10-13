pieroxy/http-front-server

commit 52fb520a8f441628e190ffac29081ee7d7b92861

🐛 More than one cookie can be present under the same name

src/main/java/com/nullbird/hfs/config/rules/matchers/ContainsCookie.java
@@ -1,10 +1,11 @@
 package com.nullbird.hfs.config.rules.matchers;
 
 import com.nullbird.hfs.config.Config;
-import com.nullbird.hfs.utils.config.RuleMatcher;
 import com.nullbird.hfs.http.HttpRequest;
+import com.nullbird.hfs.utils.config.RuleMatcher;
 import com.nullbird.hfs.utils.errors.ConfigurationException;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -31,8 +32,11 @@ public class ContainsCookie  implements RuleMatcher {
    */
   @Override
   public boolean match(HttpRequest request) {
-    String cookieValue = request.getCookieValue(this.name);
-    return cookieValue != null && pattern.matcher(cookieValue).find();
+    List<String> cookieValues = request.getCookieValues(this.name);
+    for (var cookieValue : cookieValues) {
+      if (pattern.matcher(cookieValue).find()) return true;
+    }
+    return false;
   }
 
   @Override

src/main/java/com/nullbird/hfs/config/rules/actions/BasicAuthenticate.java
@@ -64,7 +64,7 @@ public class BasicAuthenticate implements RuleAction {
 
   @Override
   public void run(HttpRequest request, HttpResponse response, RuleMatcher matcher) {
-    if (Objects.equals(request.getCookieValue(cookieName), COOKIE_VALUE)) return;
+    if (request.getCookieValues(cookieName).contains(COOKIE_VALUE)) return;
     try {
       Map<String, String> postData = request.decodeSimpleXWWWFormUrlEncodedPostData();
       String login = postData.get(AUTH_LOGIN);

src/main/java/com/nullbird/hfs/http/HttpRequest.java
@@ -3,6 +3,7 @@ package com.nullbird.hfs.http;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Map;
 
 public interface HttpRequest {
@@ -10,7 +11,13 @@ public interface HttpRequest {
   String getUrl();
   String getHeader(String name);
   String getParameter(String name);
-  String getCookieValue(String name);
+
+  /**
+   *
+   * @param name
+   * @return never null
+   */
+  List<String> getCookieValues(String name);
 
   byte[] getBodyAsByteArray() throws IOException;
 

src/main/java/com/nullbird/hfs/http/ServletHttpRequest.java
@@ -1,7 +1,6 @@
 package com.nullbird.hfs.http;
 
 import com.nullbird.hfs.utils.StreamUtils;
-import com.nullbird.hfs.utils.UtilityCollectors;
 import jakarta.servlet.AsyncContext;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,6 +14,7 @@ import java.net.URLDecoder;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class ServletHttpRequest implements HttpRequest {
   private final static Logger LOGGER = Logger.getLogger(ServletHttpRequest.class.getName());
@@ -106,14 +106,14 @@ public class ServletHttpRequest implements HttpRequest {
   }
 
   @Override
-  public String getCookieValue(String name) {
+  public List<String> getCookieValues(String name) {
     if (LOGGER.isLoggable(Level.FINER)) LOGGER.finer("getCookieValue("+name+") :: not computing");
     var cookies = request.getCookies();
     if (cookies == null) return null;
-    Cookie c = Arrays.stream(cookies)
+    return Arrays.stream(cookies)
             .filter(cookie -> Objects.equals(cookie.getName(), name))
-            .collect(UtilityCollectors.getOneItemOrNull());
-    return c == null ? null : c.getValue();
+            .map(Cookie::getValue)
+            .collect(Collectors.toList());
   }
 
   @Override

src/main/java/com/nullbird/hfs/utils/UtilityCollectors.java
@@ -7,14 +7,23 @@ public class UtilityCollectors {
   // From https://stackoverflow.com/questions/22694884/filter-java-stream-to-1-and-only-1-element
   public static <T> Collector<T, ?, T> getOneItemOrNull() {
     return Collectors.collectingAndThen(
-        Collectors.toList(),
-        list -> {
-            if (list.size()==0) return null;
-          if (list.size() != 1) {
-            throw new IllegalStateException();
-          }
-          return list.get(0);
-        }
+            Collectors.toList(),
+            list -> {
+              if (list.size()==0) return null;
+              if (list.size() != 1) {
+                throw new IllegalStateException();
+              }
+              return list.get(0);
+            }
+    );
+  }
+  public static <T> Collector<T, ?, T> getFirstItemOrNull() {
+    return Collectors.collectingAndThen(
+            Collectors.toList(),
+            list -> {
+              if (list.size()==0) return null;
+              return list.get(0);
+            }
     );
   }
 }

commit a8dab0cde207ca62c747846c9ee50fb6da994d0c

🐛🚨 LAst fixes and tests from regression from commit 52fb520a

src/main/java/com/nullbird/hfs/config/rules/matchers/ContainsCookie.java
@@ -33,8 +33,10 @@ public class ContainsCookie  implements RuleMatcher {
   @Override
   public boolean match(HttpRequest request) {
     List<String> cookieValues = request.getCookieValues(this.name);
-    for (var cookieValue : cookieValues) {
-      if (pattern.matcher(cookieValue).find()) return true;
+    if (cookieValues != null) {
+      for (var cookieValue : cookieValues) {
+        if (pattern.matcher(cookieValue).find()) return true;
+      }
     }
     return false;
   }

src/test/java/com/nullbird/hfs/config/rules/actions/BasicAuthenticateTest.java
@@ -35,6 +35,22 @@ public class BasicAuthenticateTest {
   }
 
   @Test
+  public void unauthenticatedTestNoCookies() {
+    var actiom = buildAction();
+
+    var response = new TestResponse();
+    var req = TestRequest.fromUrl("http://anything/");
+    req.setCookieValues(null);
+    actiom.run(req, response, null);
+    assertEquals(401, response.getStatus());
+    assertNotNull(response.getBody());
+    String body = new String(response.getBody(), StandardCharsets.UTF_8);
+    assertTrue(body.length() > 0);
+    assertTrue(body.startsWith("<html>"));
+    assertTrue(body.contains("<input "));
+  }
+
+  @Test
   public void authenticatedTest() {
     var action = buildAction();
     var response = new TestResponse();

src/test/java/com/nullbird/hfs/config/rules/matchers/ContainsCookieTest.java
@@ -17,6 +17,7 @@ public class ContainsCookieTest extends MatcherTestCase {
     fromCookieHelper(hi, "testCookieName2", "testCookieValue", false);
     fromCookieHelper(hi, "testCookieName", "2testCookieValue2", true);
     fromCookieHelper(hi, "testCookieName", "test2CookieValue", false);
+    fromCookieHelper(hi, null, null, false);
   }
 
   @Test

src/test/java/utils/MatcherTestCase.java
@@ -14,7 +14,11 @@ public class MatcherTestCase {
 
   protected void fromCookieHelper(RuleMatcher matcher, String cookieName, String cookieValue, boolean shouldMatch) {
     var req = TestRequest.fromUrl("https://toto.com");
-    req.getCookieValues().put(cookieName, cookieValue);
+    if (cookieName == null) {
+      req.setCookieValues(null);
+    } else {
+      req.getCookieValues().put(cookieName, cookieValue);
+    }
     assertEquals(shouldMatch, matcher.match(req), "For cookie " + cookieName + ":" + cookieValue);
   }
 

src/test/java/utils/TestRequest.java
@@ -71,6 +71,7 @@ public class TestRequest implements HttpRequest {
 
   @Override
   public List<String> getCookieValues(String name) {
+    if (cookieValues == null) return null;
     var res = new ArrayList<String>();
     if (cookieValues.containsKey(name)) res.add(cookieValues.get(name));
     return res;